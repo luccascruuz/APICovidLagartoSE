6670d6596a6ea81c25cae0ba872c239b949f9e39
Luccas Ribeiro Cruz <luccas_185@hotmail.com>
2022-12-18 13:06:18 -0300

    feat: add cors in server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 import router from './routes'
 import connectDatabase from './Database/database'
+import cors from 'cors'
 import dotenv from 'dotenv'
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 connectDatabase();
