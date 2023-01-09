a903bab0ae23980741bf0e0beaf5808d949c685f
Luccas Ribeiro Cruz <luccas_185@hotmail.com>
2023-01-09 19:02:21 -0300

    refactor: add try and catch

--- src/Controllers/caseDayController.ts
+++ src/Controllers/caseDayController.ts
@@ -5,61 +5,80 @@ import caseDayModel from '../Models/caseDayModel'
 const CaseDay = {
     async index(req: Request, res: Response) {
 
-        const cases = await caseDayModel.find()
-        return res.status(200).json(cases)
+        try {
+            const cases = await caseDayModel.find()
+            return res.status(200).json(cases)
+        }
+        catch (error) {
+            return res.status(500).json({ error: error })
+        }
     },
     async totalCasesAndDeaths(req: Request, res: Response) {
-        const cases = await caseDayModel.findOne().sort({ date: -1 })
-        return res.status(200).json({
-            totalCases: cases?.total_cases,
-            totalDeaths: cases?.deaths,
-            casesToday: cases?.new_cases,
-            deathsToday: cases?.new_deaths,
-            date: cases?.date
-        })
+        try {
+            const cases = await caseDayModel.findOne().sort({ date: -1 })
+            return res.status(200).json({
+                totalCases: cases?.total_cases,
+                totalDeaths: cases?.deaths,
+                casesToday: cases?.new_cases,
+                deathsToday: cases?.new_deaths,
+                date: cases?.date
+            })
+        } catch (error) {
+            return res.status(500).json({ error: error })
+        }
     },
     async lastSevenDays(req: Request, res: Response) {
-        const lastCase = await caseDayModel.findOne().sort({ date: -1 })
+        try {
+            const lastCase = await caseDayModel.findOne().sort({ date: -1 })
 
-        const dateLastCase = format(addHours(new Date(lastCase?.date ?? ''), 3), 'yyyy/MM/dd')
+            const dateLastCase = format(addHours(new Date(lastCase?.date ?? ''), 3), 'yyyy/MM/dd')
 
-        const dateLastSevenDay = format(subDays(addHours(new Date(dateLastCase), 3), 7), 'yyyy/MM/dd')
+            const dateLastSevenDay = format(subDays(addHours(new Date(dateLastCase), 3), 7), 'yyyy/MM/dd')
 
-        const cases = await caseDayModel.find({ date: { $gte: dateLastSevenDay, $lte: dateLastCase } })
-        return res.status(200).json(cases)
+            const cases = await caseDayModel.find({ date: { $gte: dateLastSevenDay, $lte: dateLastCase } })
+            return res.status(200).json(cases)
+        }
+        catch (error) {
+            return res.status(500).json({ error: error })
+        }
     },
     async movingAverageOfCases(req: Request, res: Response) {
-        const lastCase = await caseDayModel.findOne().sort({ date: -1 })
+        try {
+            const lastCase = await caseDayModel.findOne().sort({ date: -1 })
 
-        const lastWeekNumber = lastCase?.week_number ?? 0
-        const arrayCasesForWeek = new Array(lastWeekNumber).fill(null)
+            const lastWeekNumber = lastCase?.week_number ?? 0
+            const arrayCasesForWeek = new Array(lastWeekNumber).fill(null)
 
-        let movingAverage: { movingAverage: number; date: Date }[] = []
+            let movingAverage: { movingAverage: number; date: Date }[] = []
 
-        const arrayMovitest = arrayCasesForWeek.map(async (value, index) => {
-            const casesForWeek = await caseDayModel.find({ week_number: index + 1 })
+            const arrayMovitest = arrayCasesForWeek.map(async (value, index) => {
+                const casesForWeek = await caseDayModel.find({ week_number: index + 1 })
 
-            if (casesForWeek.length > 0) {
-                const somaCasos = casesForWeek.reduce(function (totalSum, caseDay) {
-                    const numberCaseDay = caseDay.new_cases ?? 0
+                if (casesForWeek.length > 0) {
+                    const somaCasos = casesForWeek.reduce(function (totalSum, caseDay) {
+                        const numberCaseDay = caseDay.new_cases ?? 0
 
-                    return totalSum + numberCaseDay
-                }, 0)
+                        return totalSum + numberCaseDay
+                    }, 0)
 
-                movingAverage.push({
-                    movingAverage: Math.round(somaCasos / casesForWeek.length),
-                    date: casesForWeek[casesForWeek.length - 1].date ?? new Date()
-                })
-            }
-        })
+                    movingAverage.push({
+                        movingAverage: Math.round(somaCasos / casesForWeek.length),
+                        date: casesForWeek[casesForWeek.length - 1].date ?? new Date()
+                    })
+                }
+            })
 
-        await Promise.all(arrayMovitest)
+            await Promise.all(arrayMovitest)
 
-        movingAverage.sort(function (a, b) {
-            return a.date?.getTime() - b.date?.getTime()
-        })
+            movingAverage.sort(function (a, b) {
+                return a.date?.getTime() - b.date?.getTime()
+            })
 
-        return res.status(200).json(movingAverage)
+            return res.status(200).json(movingAverage)
+        }
+        catch (error) {
+            return res.status(500).json({ error: error })
+        }
 
     },
     async addCaseDay(req: Request, res: Response) {
@@ -82,7 +101,7 @@ const CaseDay = {
             const caseDay = await caseDayModel.create(req.body)
             return res.status(201).json({ message: "Caso do dia adicionado com sucesso", data: caseDay })
         } catch (error) {
-            res.status(500).json({ error: error })
+            return res.status(500).json({ error: error })
         }
     },
 }
