e017c0df364bd187042286c931f2851e3698ba0a
Luccas Ribeiro Cruz <luccas_185@hotmail.com>
2023-01-12 16:19:45 -0300

    chore: build project

--- build/Controllers/vaccinationController.js
+++ build/Controllers/vaccinationController.js
@@ -18,13 +18,12 @@ const Vaccination = {
         return __awaiter(this, void 0, void 0, function* () {
             try {
                 const page = req.query.page ? Number(req.query.page) : 1;
-                const dose = req.query.dose ? { dose: Number(req.query.dose) } : {};
-                const vaccinationsLength = (yield vaccinationModel_1.default.find(dose)).length;
-                const limit = 50;
+                const vaccinationsLength = yield vaccinationModel_1.default.find().count();
+                const limit = 100;
                 const totalPages = Math.ceil(vaccinationsLength / limit);
                 const skip = limit * (page - 1);
                 const vaccinations = yield vaccinationModel_1.default
-                    .find(dose)
+                    .find()
                     .skip(skip)
                     .limit(limit);
                 return res
@@ -44,16 +43,18 @@ const Vaccination = {
     totalDosesApplied(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
             try {
-                const vaccinationsLength = (yield vaccinationModel_1.default.find()).length;
-                const vaccinationsFirstDoseLength = (yield vaccinationModel_1.default.find({ dose: 1 })).length;
-                const vaccinationsSecondDoseLength = (yield vaccinationModel_1.default.find({ dose: 2 })).length;
-                const vaccinationsThirdDoseLength = (yield vaccinationModel_1.default.find({ dose: 3 })).length;
-                const vaccinationsFourthDoseLength = (yield vaccinationModel_1.default.find({ dose: 4 })).length;
+                const vaccinationsFirstDoseLength = yield vaccinationModel_1.default.find({ vacina_descricao_dose: "1ª Dose" }).count();
+                const vaccinationsSecondDoseLength = yield vaccinationModel_1.default.find({ vacina_descricao_dose: "2ª Dose" }).count();
+                const vaccinationsThirdDoseLength = yield vaccinationModel_1.default.find({ vacina_descricao_dose: "Reforço" }).count();
+                const vaccinationsFourthDoseLength = yield vaccinationModel_1.default.find({ vacina_descricao_dose: "2º Reforço" }).count();
+                const vaccinationsFifthDoseLength = yield vaccinationModel_1.default.find({ vacina_descricao_dose: "3º Reforço" }).count();
+                const vaccinationsLength = yield vaccinationModel_1.default.find().count();
                 const totalDoses = {
                     firstDose: vaccinationsFirstDoseLength,
                     secondDose: vaccinationsSecondDoseLength,
                     thirdDose: vaccinationsThirdDoseLength,
                     fourthDose: vaccinationsFourthDoseLength,
+                    fifthDose: vaccinationsFifthDoseLength,
                     totalDoses: vaccinationsLength
                 };
                 return res.status(200).json(totalDoses);
@@ -65,7 +66,7 @@ const Vaccination = {
     },
     addVaccination(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
-            const { date, state, city, ibgeID, dose, vaccine, sex, age, count, pop2021, } = req.body;
+            const { document_id, paciente_id, paciente_idade, paciente_dataNascimento, paciente_enumSexoBiologico, paciente_racaCor_codigo, paciente_racaCor_valor, paciente_endereco_coIbgeMunicipio, paciente_endereco_coPais, paciente_endereco_nmMunicipio, paciente_endereco_nmPais, paciente_endereco_uf, paciente_endereco_cep, paciente_nacionalidade_enumNacionalidade, estabelecimento_valor, estabelecimento_razaoSocial, estalecimento_noFantasia, estabelecimento_municipio_codigo, estabelecimento_municipio_nome, estabelecimento_uf, vacina_grupoAtendimento_codigo, vacina_grupoAtendimento_nome, vacina_categoria_codigo, vacina_categoria_nome, vacina_lote, vacina_fabricante_nome, vacina_fabricante_referencia, vacina_dataAplicacao, vacina_descricao_dose, vacina_codigo, vacina_nome, sistema_origem } = req.body;
             try {
                 const vaccination = yield vaccinationModel_1.default.create(req.body);
                 return res
